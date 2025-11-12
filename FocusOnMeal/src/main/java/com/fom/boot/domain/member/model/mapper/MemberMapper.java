ce897a0a6a9d10c7e7f5aa6a5e78fd5ae9b2b02f
DESKTOP-DO8C9CL\성주애 <tjdwndo5563@gmail.com>
2025-11-12 15:40:50 +0900

    [ADD] 로그인을 만들었다.
    1. 로그인을 할 수 있도록 만들었다.
    2. 암호화 비밀번호를 가지고도 로그인을 할 수 있도록 한다.
    3. 비밀번호 암호화 파일을 만들었다.
    4. 리액트로 로그인 화면을 구현했다.
    5. 일반회원인지 관리자인지 구분하는 코드를 작성했다.

--- FocusOnMeal/src/main/java/com/fom/boot/domain/member/model/mapper/MemberMapper.java
+++ FocusOnMeal/src/main/java/com/fom/boot/domain/member/model/mapper/MemberMapper.java
@@ -2,7 +2,12 @@ package com.fom.boot.domain.member.model.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.fom.boot.app.member.dto.LoginRequest;
+import com.fom.boot.domain.member.model.vo.Member;
+
 @Mapper
 public interface MemberMapper {
+
+	Member selectOneByLogin(LoginRequest member);
 
 }
