e8347b3bf41580e65a431e37aeccd098de2323e9
zhzhzlajava <zhzhzlajava@gmail.com>
2025-11-13 16:37:45 +0900

    [MODIFY] App.jsx 주석 추가
    - 편의성을 위해 주석 추가했습니다. 이왕이면 맞춰서 해주세욤..

--- FocusOnMeal/src/main/front-end/src/App.jsx
+++ FocusOnMeal/src/main/front-end/src/App.jsx
@@ -5,6 +5,7 @@ import IngredientLayout from './components/IngredientLayout';
 import IngredientSearch from './pages/ingredient/list';
 import NoticeList from './pages/board/notice/NoticeList';
 import Header from "./components/common/Header";
+import Dashboard from './pages/mypage/Dashboard';
 
 
 function App() {
@@ -13,14 +14,26 @@ function App() {
     <> 
       <Header />
       <Routes>
-        <Route path="/member/login" element={<Login />} />
+        {/* 메인페이지 */}
         <Route path="/" element={<div>홈페이지</div>} />
+
+        {/* 회원 관련 */}
+        <Route path="/member/login" element={<Login />} />
+
+        {/* 마이페이지 관련 */}
+        <Route path="/mypage" element={<Dashboard />} />
+
+        {/* 식자재 관련 */}
         <Route path="/ingredient" element={<IngredientLayout />}>
           <Route index element={<IngredientSearch />} /> 
           <Route path="list" element={<IngredientSearch />} /> 
         </Route>
+
+        {/* 공지사항 게시판 관련 */}
         <Route path="/board/notice/list" element={<NoticeList /> }/>
         {/* <Route path="/notice/detail" element={<NoticeDetail />} /> */}
+
+        {/* 안전정보 게시판 관련 */}
       </Routes>
     </>
   );
